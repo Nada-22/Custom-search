e79bb6707c75d9b2cb40590bced0e79737416fde
nada.h <nada.h22425@gmail.com>
2022-06-22 18:28:42 +0200

    get posts and install bootstrap

--- search-Task/src/app/app-routing.module.ts
+++ search-Task/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './componants/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PostsComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ search-Task/src/app/componants/posts/posts.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts: Post[] = [];
+  constructor(private _posts:PostsService) { }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+  getPosts(): void{
+    this._posts.getPosts().subscribe(
+      (res: any) => {
+        this.posts = res;
+        console.log(this.posts);
+        
+      }, (err: any) => {
+        console.log(err);
+        
+    }
+  )
+}
+}
